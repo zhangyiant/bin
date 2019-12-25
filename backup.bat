1952ed1d4ef00d0424b472440730df9a08633266
Yi Zhang <zhangyiant@hotmail.com>
2019-12-25 11:08:02 +0800

    Add backup3 script.

--- backup.bat
+++ backup.bat
@@ -1,1 +1,3 @@
-robocopy C:\Users\zhang\Documents W:\LaptopBackup\DellDocuments /s
+robocopy C:\Users\zhang\Documents\MyDocuments W:\LaptopBackup\DellDocuments /s
+robocopy C:\Users\zhang\Pictures\PicBackup3 W:\LaptopBackup\DellPictures\PicBackup3 /s
+
