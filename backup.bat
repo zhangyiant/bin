da87b125290dad843c5336286b9d4a7ac75d70ec
Yi Zhang <zhangyiant@hotmail.com>
2020-01-30 09:43:22 +0800

    Update alias.

--- backup.bat
+++ backup.bat
@@ -1,3 +1,4 @@
 robocopy C:\Users\zhang\Documents\MyDocuments W:\LaptopBackup\DellDocuments /s
 robocopy C:\Users\zhang\Pictures\PicBackup3 W:\LaptopBackup\DellPictures\PicBackup3 /s
+robocopy C:\Users\zhang\Videos\VideoBackup2 W:\LaptopBackup\DellVideos\VideoBackup2 /s
 
