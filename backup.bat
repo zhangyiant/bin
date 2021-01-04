69a4733174228c82d4558ee6e844a132272110a8
Yi Zhang <zhangyiant@hotmail.com>
2021-01-04 21:00:22 +0800

    Update

--- backup.bat
+++ backup.bat
@@ -1,4 +1,4 @@
-robocopy C:\Users\zhang\Documents\MyDocuments W:\LaptopBackup\DellDocuments /s
-robocopy C:\Users\zhang\Pictures\PicBackup3 W:\LaptopBackup\DellPictures\PicBackup3 /s
-robocopy C:\Users\zhang\Videos\VideoBackup2 W:\LaptopBackup\DellVideos\VideoBackup2 /s
+robocopy C:\Users\zhang\Documents\MyDocuments W:\LaptopBackup\DellDocuments2 /s
+robocopy C:\Users\zhang\Pictures\PicBackup4 W:\LaptopBackup\DellPictures\PicBackup4 /s
+robocopy C:\Users\zhang\Videos\VideoBackup3 W:\LaptopBackup\DellVideos\VideoBackup3 /s
 
