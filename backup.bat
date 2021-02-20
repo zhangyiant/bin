b4b27a0cb5c8d6d67e20cb7ddc7c38cb2c974026
Yi Zhang <zhangyiant@hotmail.com>
2021-02-20 20:32:45 +0800

    Update backup script.

--- backup.bat
+++ backup.bat
@@ -1,4 +1,3 @@
-robocopy C:\Users\zhang\Documents\MyDocuments W:\LaptopBackup\DellDocuments2 /s
-robocopy C:\Users\zhang\Pictures\PicBackup4 W:\LaptopBackup\DellPictures\PicBackup4 /s
-robocopy C:\Users\zhang\Videos\VideoBackup3 W:\LaptopBackup\DellVideos\VideoBackup3 /s
-
+robocopy C:\Users\zhang\Documents\MyDocuments \\192.168.2.238\nas\Documents /e
+robocopy C:\Users\zhang\Pictures\Pictures \\192.168.2.238\nas\Pictures /e
+robocopy C:\Users\zhang\Videos\Videos \\192.168.2.238\nas\Videos /e
