bf39bcfd40b0b57316cb42cf144c88caf68978f3
Yi Zhang <zhangyiant@hotmail.com>
2022-05-22 16:38:46 +0800

    update

--- backup.bat
+++ backup.bat
@@ -1,3 +1,3 @@
-robocopy C:\Users\zhang\Documents\MyDocuments \\192.168.2.238\nas\Documents /e
-robocopy C:\Users\zhang\Pictures\Pictures \\192.168.2.238\nas\Pictures /e
-robocopy C:\Users\zhang\Videos\Videos \\192.168.2.238\nas\Videos /e
+robocopy C:\Users\zhang\Documents\MyDocuments \\192.168.1.238\nas\Documents /e
+robocopy C:\Users\zhang\Pictures\Pictures \\192.168.1.238\nas\Pictures /e
+robocopy C:\Users\zhang\Videos\Videos \\192.168.1.238\nas\Videos /e
